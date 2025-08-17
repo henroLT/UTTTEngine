ccde659323afb087a0d4bac704cf6c8a0f6f5a98
Your Name <zhao.henry14@gmail.com>
2025-08-17 18:34:43 -0500

    debugging minimax

--- 3x3/headers/headerImpls/solver.cpp
+++ 3x3/headers/headerImpls/solver.cpp
@@ -1,4 +1,6 @@
+#pragma once
 #include "../solver.hpp"
+#include <fstream>
 
 /*
 private:
@@ -56,7 +58,7 @@ void Solver::threadFunc(lfqueue *list, std::unordered_map<state, stateTree*> &vi
 }
 
 std::vector<stateTree*> Solver::generateChildren(stateTree* parent) {
-    if (checkWin(parent->val.board) || checkDraw(parent->val.board)) return {};
+    if (eval(parent->val) != -2) return {};
     
     std::vector<stateTree*> res;
 
@@ -96,31 +98,53 @@ int Solver::eval(const state& s) {
     if (board[0][2] != '\0' && board[0][2] == board[1][1] && board[1][1] == board[2][0])
         return board[0][2] != PLAYER ? +1 : -1;
 
-    return 0;
+    bool full = true;
+    for (int i = 0; i < SIZE && full; ++i)
+        for (int j = 0; j < SIZE && full; ++j)
+            if (board[i][j] == '\0')
+                full = false;
+
+    if (full) return 0;
+    
+    return -2;
 }
 
-void Solver::weighPaths(stateTree* node, std::unordered_map<state, bool>& seen) {  
+void Solver::weighPaths(stateTree* node, std::unordered_map<state, bool>& seen,  std::ofstream* out) {  
     state dummy = node->val;
     if (seen[dummy]) return;
-    for (auto& c : node->children) weighPaths(c, seen);
+    seen[node->val] = true;
+
+    for (auto& c : node->children) weighPaths(c, seen, out);
 
     int scr = eval(dummy);
-    if (scr != 0) node->score = static_cast<double>(scr);
+    if (scr != -2) node->score = static_cast<double>(scr);
     else {
         bool compTurn = (node->turn % 2 == 0)? (PLAYER == 'X') : (PLAYER == 'O');
         auto it = node->children.begin();
         node->score = (*it++)->score;
             
-        if (compTurn) {
+        if (compTurn) 
             for (; it != node->children.end(); ++it)
                 node->score = std::max(node->score, (*it)->score);
-        } else {
+
+        else 
             for (; it != node->children.end(); ++it)
                 node->score = std::min(node->score, (*it)->score);
-        }
+        
     }
-    
-    seen[node->val] = true;
+
+    //// --- Print board and score ---
+    //if (out) { // only write if file pointer provided
+    //    *out << "Board (turn " << node->turn << "):\n";
+    //    for (int i = 0; i < SIZE; ++i) {
+    //        for (int j = 0; j < SIZE; ++j) {
+    //            char c = node->val.board[i][j];
+    //            *out << (c == '\0' ? '.' : c) << ' ';
+    //        }
+    //        *out << '\n';
+    //    }
+    //    *out << "Score: " << node->score << "\n\n";
+    //}
 }
 
 
@@ -154,25 +178,10 @@ void Solver::generateStates() {
 }
 
 void Solver::startWeighPaths(std::unordered_map<state, bool>& seen) {
-    weighPaths(head, seen);
+    std::ofstream file("output.txt");
+    weighPaths(head, seen, &file);
+    file.close();
 
-    state a;
-    for (int i = 0; i < SIZE; ++i)
-        for (int j = 0; j < SIZE; ++j)
-            a.board[i][j] = '\0';
-
-    a.board[0][0] = 'X';
-    a.board[0][1] = 'X';
-    a.board[1][0] = 'O';
-    a.board[1][1] = 'O';
-    
-    auto it = visit.find(a);
-    if (it != visit.end() && it->second != nullptr) {
-        std::cout << it->second->score << std::endl;
-    } else {
-        std::cout << "State not in tree yet!" << std::endl;
-    }
-    std::cin.get();
 }
 
 std::pair<int,int> Solver::chooseBest(const state &s) {
--- 3x3/src/runner.cpp
+++ 3x3/src/runner.cpp
@@ -87,11 +87,11 @@ void run(bool& weighedPaths) {
         std::cout << "Invalid input. Please enter \"X\" or \"O\",\n";
     } while (1);
     
-    //if (!weighedPaths) {
-    //    std::unordered_map<state, bool> dummyVisit;
-    //    solver->startWeighPaths(dummyVisit);
-    //    weighedPaths = true;
-    //}
+    if (!weighedPaths) {
+        std::unordered_map<state, bool> dummyVisit;
+        solver->startWeighPaths(dummyVisit);
+        weighedPaths = true;
+    }
 
     while (1) {
         boardDisplay();
